3377afaf5c1da1afd901c28fd2e1cc40ec120080
JonasTadeu023 <jonas.saldanha218@gmail.com>
2019-11-29 20:18:16 -0300

    cadastro de representantes

---
+++ includes/register_user.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    include('../functions/dbh.php');
+
+    $name = mysqli_real_escape_string($conn, trim($_POST['name']));
+    $cpf = mysqli_real_escape_string($conn, trim($_POST['cpf']));
+    $email = mysqli_real_escape_string($conn, trim($_POST['email']));
+    $phone = mysqli_real_escape_string($conn, trim($_POST['phone']));
+    $pwd = mysqli_real_escape_string($conn, trim(md5($_POST['pwd'])));
+    $image = $_FILES['arquivo']['name'];
+
+    $sql = "SELECT count(*) as total from user where user_cpf = '$cpf'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    if ($row['total'] == 1) {
+      $_SESSION['Usuario com este CPF já existe!!!'] = true;
+      header('location: ../index.html');
+      $conn->close();
+      exit();
+    }
+
+    $result = "INSERT INTO user (user_name, user_cpf, user_email, user_phone, user_photo, user_adm, user_pwd) 
+    VALUES ('$name', '$cpf', '$email', '$phone', '$image','1', '$pwd'";
+    $result_perfil = mysqli_query($conn, $result);
+
+    $user = '../database/user/'.$cpf.'/';
+    mkdir($user, 0777);
+
+    if (move_uploaded_file($_FILES['arquivo']['tmp_name'],$user.$image)) {
+      if ($conn->query($sql) == TRUE) {
+          $_SESSION['Cadastro concluido com sucesso!!!'] = true;
+        }
+        $conn->close();
+        header('location: ../templates/login.php');
+        exit();
+    }
+?>
--- public/register_user.php
+++ public/register_user.php
@@ -6,14 +6,17 @@
 
 <form action="../includes/register_user.php">
     <p>Nome do representante</p>
-    <input type="text" placeholder="José de Alencar"><br>
+    <input type="text" placeholder="José de Alencar" name="name"><br>
     <p>CPF do representante</p>
-    <input type="number" placeholder="04199078070"><br>
+    <input type="number" placeholder="04199078070" name="cpf"><br>
     <p>E-mail do representante</p>
-    <input type="text" placeholder="jose@gmail.com"><br>
+    <input type="text" placeholder="jose@gmail.com" name="email"><br>
     <p>Número de celular do representante</p>
-    <input type="number" placeholder="55 99156020"><br>
+    <input type="number" placeholder="55 99156020" name="phone"><br>
+    <p>Foto de perfil do representante</p>
+    <input type="file" placeholder="55 99156020" name="phone"><br>
     <p>Senha do representante</p>
-    <input type="text"><br>
+    <input type="text" name="pwd"><br>
     <input type="submit" value="Cadastrar representante">
 </form>
+</body>
