3e2475d82d88bc8bbe8765bf28ef5baf81937f88
joao_danilo <jddiedrich@gmail.com>
2019-11-29 21:46:30 -0300

    todas as tabelas prontas menos caminhao

--- sql/delivery.sql
+++ sql/delivery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 30-Nov-2019 às 01:04
+-- Generation Time: 30-Nov-2019 às 01:40
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 7.3.3
 
--- sql/order.sql
+++ sql/order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 30-Nov-2019 às 01:15
+-- Generation Time: 30-Nov-2019 às 01:41
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 7.3.3
 
