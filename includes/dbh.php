4d017a01acc91f64a1fe199ce8ea3179d6381613
DaddySyres <darivafilho@gmail.com>
2019-11-29 21:12:35 -0300

    outro commit

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,7 +1,7 @@
 <?php
     define('HOST', 'localhost');
-    define('USUARIO', 'dar');
-    define('SENHA', 'usbw');
+    define('USUARIO', 'root');
+    define('SENHA', '');
     define('DB', 'bakof_bd');
 
     $conn = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Não foi possivel conectar-se ao banco de dados');
